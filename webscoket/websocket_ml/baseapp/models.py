182f5d6f5714ef10308db8fa409c08a0c06a198c
Aman Verma <66964572+Aman-Verma-28@users.noreply.github.com>
2022-06-24 13:18:28 +0530

    Update models.py

--- webscoket/websocket_ml/baseapp/models.py
+++ webscoket/websocket_ml/baseapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+import datetime
+
+class Reminder(models.Model):
+    time = models.DateTimeField(auto_created=True)
+    doctor = models.CharField(max_length=20)
+    patient = models.CharField(max_length=20)
+    link = models.CharField(max_length=20)
+    doctor_id = models.CharField(max_length=20)
