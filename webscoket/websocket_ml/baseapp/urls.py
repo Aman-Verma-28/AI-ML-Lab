feaa9d3ee3e96be875edac0b50271d7c9fa3b73c
Aman-Verma-28 <aman@infilect.com>
2022-07-24 07:56:11 +0000

    backend for turnitin

--- webscoket/websocket_ml/baseapp/urls.py
+++ webscoket/websocket_ml/baseapp/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('api/youtube/v1',  YoutubeAPI.as_view(), name='youtubeAPI'),
     path('api/calendar/v1',  CalendarAPI.as_view(), name='calendarAPI'),
-    path('test', TestView.as_view())
+    path('test', TestView.as_view()),
+    path('turnitin/', TurnitinView.as_view())
 
 ]
